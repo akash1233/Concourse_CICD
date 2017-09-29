ddbf3a0e5f09db3f31430268f8919d7d371a8f72
dxs8002 <dharmendra_singh@homedepot.com>
2017-09-29 15:35:05 -0400

    added the llc pipeline.

---
+++ scripts/func-servicetest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export https_proxy=http://thd-svr-proxy-qa.homedepot.com:7070
+
+set -e -x
+
+ls -al
+
+cd ../code-repo
+
+./gradlew funcIntegrationTest
+
+chmod 777 $TEST_OUTPUT_DIRECTORY
+
+ls -al ./$TEST_OUTPUT_DIRECTORY/
+
+cp -a ./$TEST_OUTPUT_DIRECTORY/. ../tests
+
+ls -l
--- scripts/func-test.sh
+++ scripts/func-test.sh
@@ -2,7 +2,7 @@
 set -e -x
 export https_proxy=http://thd-svr-proxy-qa.homedepot.com:7070
 ls -las
-cd code-repo-dct/iom-ui/
+cd ../code-repo/iom-ui/
 #wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 #sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 #apt-get update
