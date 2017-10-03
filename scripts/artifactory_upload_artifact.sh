d3f7782e4541f5d11f92d33194c5e0941ab0c091
dxs8002 <dharmendra_singh@homedepot.com>
2017-10-03 13:55:21 -0400

    testing dev pipeline.

--- scripts/artifactory_upload_artifact.sh
+++ scripts/artifactory_upload_artifact.sh
@@ -4,7 +4,10 @@ source "${BASH_SOURCE%/*}/flow-env.sh"
 
 set -e -x
 
-export https_proxy=http://thd-svr-proxy-qa.homedepot.com:7070
+#export https_proxy=http://thd-svr-proxy-qa.homedepot.com:7070
+
+
+ls -al
 
 echo "Artifactory upload"
 
