aa8535cc1341a1cb1bb1cfa78a42c81813664381
dxs8002 <dharmendra_singh@homedepot.com>
2017-10-05 16:44:34 -0400

    fixed paths.

--- scripts/yarn_build.sh
+++ scripts/yarn_build.sh
@@ -7,7 +7,7 @@ npm install -g yarn
 
 buildcopy(){
 cd ./$1
-mv ../../repo-cache/node_modules node_modules
+cp ../../repo-cache/node_modules node_modules
 npm rebuild
 npm install
 yarn run build:${BUILD_TARGET}
