24dc6e25c2ba70d8ab1deab50badda89da4ef527
dxs8002 <dharmendra_singh@homedepot.com>
2017-10-05 17:54:00 -0400

    fixed paths.

--- scripts/yarn_build.sh
+++ scripts/yarn_build.sh
@@ -6,21 +6,21 @@ npm install -g yarn
 
 
 buildcopy(){
-cd ./$1
-cp -r ../../repo-cache/node_modules node_modules
-npm rebuild
-npm install
-yarn run build:${BUILD_TARGET}
-chmod 777 $DIST_DIRECTORY
-mkdir -p ../../dist/$1/ && chmod 777 ../../dist/$1/
-cp -r ./$DIST_DIRECTORY/* ../../dist/$1/
-ls -lrt ../../dist/$1/
+    cd ./$1
+    cp -r ../../repo-cache/node_modules node_modules
+    npm rebuild
+    npm install
+    yarn run build:${BUILD_TARGET}
+    chmod 777 $DIST_DIRECTORY
+    mkdir -p ../../dist/$1/ && chmod 777 ../../dist/$1/
+    cp -r ./$DIST_DIRECTORY/* ../../dist/$1/
+    ls -lrt ../../dist/$1/
 }
 
 buildcopy iom-ui
 cd ..
 buildcopy iom-approval-ui
-ls -lrt ../dist/
+
 ls -lrt ../../dist/iom-ui
 ls -lrt ../../dist/iom-approval-ui
 
