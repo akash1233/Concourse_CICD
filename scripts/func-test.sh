00acb9cd52d85cea18e7570c04f0360deb3a2f84
dxs8002 <dharmendra_singh@homedepot.com>
2017-10-06 15:08:49 -0400

    fixed all the known issue.

--- scripts/func-test.sh
+++ scripts/func-test.sh
@@ -2,14 +2,11 @@
 set -e -x
 export https_proxy=http://thd-svr-proxy-qa.homedepot.com:7070
 ls -las
-cd ../code-repo/iom-ui/
-#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-#sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-#apt-get update
-#apt-get install -y google-chrome-stable
-#echo "compiling the tsc file "
+cd ./iom-ui/
+cp -r ../../repo-cache/node_modules node_modules
 npm install -g yarn
-npm install -s
+npm rebuild
+npm install
 yarn run e2e
 #protractor --baseUrl=$BASE_URL
 chmod 777 functional_results
