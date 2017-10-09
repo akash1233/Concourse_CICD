3cd7801c77c2bca0baf6f2eb8834c4ad0f249b7b
dxs8002 <dharmendra_singh@homedepot.com>
2017-10-09 15:32:50 -0400

    updated locsmith conf's

--- scripts/locksmith.sh
+++ scripts/locksmith.sh
@@ -2,7 +2,28 @@
 
 source "${BASH_SOURCE%/*}/flow-env.sh"
 
-set -e -x
+if [[ -z "${LOCKSMITH_URL}" ]]; then
+  echo "LOCKSMITH_URL is not set, defaulting to http://locksmith-api.apps-zb.homedepot.com/api/grants"
+  LOCKSMITH_URL="http://locksmith-api.apps-zb.homedepot.com/api/grants"
+fi
+ CHG_NUMBER=`CHG0300505`
+  # get CHG # from environment (e.g. $CHG_NUMBER)
+  echo "invoking LockSmith for $CHG_NUMBER"
 
-echo "mock response from the locksmith-service"
+  set -x
+  RESPONSE=$(curl -i -X POST \
+    -H "accept: application/json" \
+    -H "content-type: application/json" \
+    -d "{ \"service_now_record\": \"$CHG_NUMBER\"}" \
+    "$LOCKSMITH_URL")
+  set +x
 
+  STATUS=$(echo $RESPONSE | grep HTTP |  awk '{print $2}')
+
+  if [ "$STATUS" -eq "200" ] || [ "$STATUS" -eq "201" ]; then
+      echo "success: ${RESPONSE}";
+      exit;
+  else
+      echo "failed locksmith validation: ${RESPONSE}";
+      exit 1;
+  fi
