db5242a1f5513af6bbc633b4077a428a2472c485
dxs8002 <dharmendra_singh@homedepot.com>
2017-10-24 13:26:06 -0400

    testing thee blue green deployments[#151737494]

--- scripts/cloud_foundry_deploy_bg.sh
+++ scripts/cloud_foundry_deploy_bg.sh
@@ -8,127 +8,25 @@ set -e -x
 echo "Deployment repo content"
 ls -lrt ../deploy-repo
 
-echo "installing cf cli"
-
-wget -qO- 'https://cli.run.pivotal.io/stable?release=linux64-binary&source=github' | tar -zxf - -C /usr/local/bin
-
-echo "deploying apps to ZONE ${ENVIRONMENT}"
-
-#TODO change the CF_API URL to make it consistent
-cf login -a ${CF_API} -u ${DEPLOYMENT_USER} -p ${DEPLOYMENT_PWD} -o ${CF_ORG} -s ${CF_SPACE}
-
-#echo "start db2Svc service update"
-#cf cups db2Svc -p '{"uri": "'${DB2_FULL_URL}'"}' || cf uups db2Svc -p '{"uri": "'${DB2_FULL_URL}'"}'
-#echo "end db2Svc service update"
-#
-#echo "start dcmSvc service update"
-#cf cups dcmSvc -p '{"url": "'${TERADATA_FULL_URL}'"}' || cf uups dcmSvc -p '{"url": "'${TERADATA_FULL_URL}'"}'
-#echo "end dcmSvc service update"
-#
-#echo "start orcaSvc service update"
-#cf cups orcaSvc -p '{"jdbcUrl": "'${ORACLE_FULL_URL}'"}' || cf uups orcaSvc -p '{"jdbcUrl": "'${ORACLE_FULL_URL}'"}'
-#echo "end orcaSvc service update"
 
 
-echo "**************************************"
-echo "deploy started  for iom ui"
-echo "**************************************"
+echo "Deployment repo content"
+ls -lrt ../deploy-repo
 
-cd ../code-repo/iom-ui
-
-ls -ltr
-
-# TODO move the build of UI project to gradle build
-
-echo "building the project"
-
-npm install -s && npm install -g yarn
-
-yarn run build:${BUILD_TARGET}
-
-
-# Call the bg deploy ui
-bg_deploy_ui
-
-echo "**************************************"
-echo "deploy completed  for iom ui"
-echo "**************************************"
+ls -lrt ../../
 
 
 
-# Deployment for approval ui
-echo "**************************************"
-echo "deploy started  for iom approval ui"
-echo "**************************************"
+installcfcli() {
+    echo "installing cf cli"
+    wget -qO- 'https://cli.run.pivotal.io/stable?release=linux64-binary&source=github' | tar -zxf - -C /usr/local/bin
+    echo "deploying apps to ZONE ${ENVIRONMENT}"
+}
 
-cd ../iom-approval-ui
-
-
-
-ls -ltr
-
-echo "building the project"
-
-npm install -s && npm install -g yarn
-
-yarn run build:${BUILD_TARGET}
-
-
-# Call the bg deploy ui
-bg_deploy_ui
-
-echo "**************************************"
-echo "deploy completed  for iom approval ui"
-echo "**************************************"
-
-
-echo "**************************************"
-echo "deploy started  for iom-ui-service"
-echo "**************************************"
-
-cd ../iom-ui-services
-bg_deploy ../../deploy-repo/iom-ui-services.jar
-
-echo "**************************************"
-echo "deploy completed for iom-ui-service"
-echo "**************************************"
-
-echo "**************************************"
-echo "deploy started  for iom-xfer-service"
-echo "**************************************"
-cd ../iom-xfer-services
-bg_deploy ../../deploy-repo/iom-xfer-services.jar
-
-echo "**************************************"
-echo "deploy completed for iom-xfer-service"
-echo "**************************************"
-
-echo "**************************************"
-echo "deploy started  for iom-scheduler"
-echo "**************************************"
-cd ../iom-scheduler
-bg_deploy ../../deploy-repo/iom-scheduler.jar
-
-echo "**************************************"
-echo "deploy completed for iom-scheduler"
-echo "**************************************"
-
-echo "**************************************"
-echo "deploy started  for iom-approval-service"
-echo "**************************************"
-cd ../iom-approval-service
-bg_deploy ../../deploy-repo/iom-approval-service.jar
-
-echo "******************************************"
-echo "deploy completed  for iom-approval-service"
-echo "*****************************************"
-
-echo "*****************************************************************************"
-echo "Start list all the application in current space"
-cf apps
-echo "Complited list all the application in current space"
-
-echo "********************************************************* ${ENVIRONMENT} done"
+logintoconcourse() {
+    echo " logging to concourse with following api ${CF_API} and org ${CF_ORG} and  space ${CF_SPACE}"
+    cf login -a "${CF_API}" -u "${DEPLOYMENT_USER}" -p "${DEPLOYMENT_PWD}" -o "${CF_ORG}" -s "${CF_SPACE}"
+}
 
 
 
@@ -184,13 +82,13 @@ cf logout
 # BG Setup for services
 bg_deploy() {
 PATH=$1
-APP_NAME=$(awk '/name:/ {print $NF}' ${ENVIRONMENT}.manifest.yml)  # grab the app name from the manifest.yml file
+APP_NAME=$(awk '/name:/ {print $NF}' $1)  # grab the app name from the manifest.yml file
 APP_NAME_BLUE=${APP_NAME}-BLUE
 APP_NAME_GREEN=${APP_NAME}-GREEN
 CF_DOMAIN="$(echo $CF_API | cut -d '-' -f 2)"
 
 # get the app name from manifest file
-ROUTE_NAME=$(awk '/host:/ {print $NF}' ${ENVIRONMENT}.manifest.yml)  # grab the route name from the manifest.yml file
+ROUTE_NAME=$(awk '/host:/ {print $NF}' $1)  # grab the route name from the manifest.yml file
 
 # get the route name
 if [[ -z ${ROUTE_NAME} ]]; then
@@ -223,8 +121,96 @@ echo "target app name: '${APP_NAME_TARGET}'"
 
 echo "cf push application"
 
-cf push ${APP_NAME_TARGET} -f ${ENVIRONMENT}.manifest.yml -p ${PATH}
+cf push ${APP_NAME_TARGET} -f $1 -p $2
 echo "cf logout"
 cf logout
 }
+
+
+deployui() {
+    # Deployment for iom ui
+    echo "deploy started  for iom ui"
+    cd ../code-repo/iom-ui
+    ls -ltr
+    mkdir -p dist  && chmod 777 dist
+    cp ../../deploy-repo/iom-ui/* ./dist/ -rf
+    ls -lrt ./dist/
+    ls -ltr
+    cf push -f ${ENVIRONMENT}.manifest.yml
+    echo "deploy completed  for iom ui"
+    # Deployment for approval ui
+    echo "deploy started  for iom approval ui"
+    cd ../iom-approval-ui
+    ls -ltr
+    mkdir -p dist  && chmod 777 dist
+    cp ../../deploy-repo/iom-approval-ui/* ./dist/ -rf
+    ls -lrt ./dist/
+    cf push -f ${ENVIRONMENT}.manifest.yml
+    echo "deploy completed  for iom approval ui"
+    echo "deployment of for the service will be done and the ui will be excluded"
+}
+
+
+
+deployservices() {
+    cd ../code-repo
+    echo "deploy started  for iom-ui-service"
+    bg_deploy "./iom-ui-services/${ENVIRONMENT}.manifest.yml" "../deploy-repo/iom-ui-services.jar"
+    echo "deploy completed for iom-ui-service"
+
+    echo "deploy started  for iom-xfer-service"
+    bg_deploy "./iom-xfer-services/${ENVIRONMENT}.manifest.yml" "../deploy-repo/iom-xfer-services.jar"
+    echo "deploy completed for iom-xfer-service"
+
+    echo "deploy started  for iom-scheduler"
+    bg_deploy  "./iom-scheduler/${ENVIRONMENT}.manifest.yml"  "../deploy-repo/iom-scheduler.jar"
+    echo "deploy completed for iom-scheduler"
+
+    echo "deploy started  for iom-approval-service"
+    bg_deploy "./iom-approval-service/${ENVIRONMENT}.manifest.yml" "../deploy-repo/iom-approval-service.jar"
+    echo "deploy completed for iom-approval-service"
+
+
+    echo "Start list all the application in current space"
+    cf apps
+    echo "Complited list all the application in current space"
+
+    echo "Start iom-ui-service environment"
+    cf env iom-ui-service
+    echo "End iom-ui-service environment"
+
+    echo "Start iom-scheduler environment"
+    cf env iom-scheduler
+    echo "End iom-scheduler environment"
+
+    echo "Start iom-xfer-service environment"
+    cf env iom-xfer-service
+    echo "End iom-xfer-service environment"
+
+    echo "Start iom-approval-service environment"
+    cf env iom-approval-service
+    echo "End iom-xfer-service environment"
+
+}
+
+installcfcli
+logintoconcourse
+
+if [ -z "${DEPLOY_TYPE}" ]
+then
+  echo "services being deployed as DEPLOY_TYPE value not set"
+  deployservices
+elif [ "${DEPLOY_TYPE}" == "SERVICES" ]
+then
+  echo "services being deployed"
+  deployservices
+elif [ "${DEPLOY_TYPE}" == "UI" ]
+then
+  echo "ui being deployed"
+  deployui
+else
+  echo "nothing being deployed DEPLOY TYPE value has incorrect value :" ${DEPLOY_TYPE}
+fi
+
+echo "********************************************************* ${ENVIRONMENT} deployments done"
 
